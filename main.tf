0b7c1d0f90b51919afc21945e9d806699aaac9de
grahamlyons <graham@grahamlyons.com>
2017-09-21 16:48:04 +0100

    404 should be a valid status for healthcheck

--- main.tf
+++ main.tf
@@ -18,14 +18,15 @@ module "404_task_definition" {
 module "404_ecs_service" {
   source = "github.com/mergermarket/tf_load_balanced_ecs_service"
 
-  name             = "${format("%s-%s-404", var.env, var.component)}"
-  container_name   = "404"
-  container_port   = "80"
-  vpc_id           = "${var.platform_config["vpc"]}"
-  task_definition  = "${module.404_task_definition.arn}"
-  desired_count    = "${var.env == "live" ? 2 : 1}"
-  alb_listener_arn = "${module.alb.alb_listener_arn}"
-  alb_arn          = "${module.alb.alb_arn}"
+  name                 = "${format("%s-%s-404", var.env, var.component)}"
+  container_name       = "404"
+  container_port       = "80"
+  vpc_id               = "${var.platform_config["vpc"]}"
+  task_definition      = "${module.404_task_definition.arn}"
+  desired_count        = "${var.env == "live" ? 2 : 1}"
+  alb_listener_arn     = "${module.alb.alb_listener_arn}"
+  alb_arn              = "${module.alb.alb_arn}"
+  health_check_matcher = "404"
 }
 
 module "alb" {
